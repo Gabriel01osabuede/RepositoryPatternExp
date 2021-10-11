7d4cc7ce2f4dc7d395e1895c006b1eabcca12d61
Gabriel01osabuede <buedgabby@gmail.com>
2021-10-11 15:39:17 +0100

    initial commit

---
+++ RepositoryPattern.WebApi/WebApi/DeveloperController.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeveloperController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult GetPopularDevelopers([FromQuery] int count)
+        {
+            var popularDevelopers = _unitOfWork.Developers.GetPopularDevelopers(count);
+            return Ok(popularDevelopers);
+        }
+
+        [HttpGet]
+        [Route("projects")]
+        public IActionResult GetProjects()
+        {
+            var projects = _unitOfWork.Projects.GetAll();
+            return Ok(projects);
+        }
+
+        [HttpPost]
+        public IActionResult AddDeveloperAndProject()
+        {
+            var developer = new Developer
+            {
+                Follower = 35,
+                Name = "MuKesh Murugan"
+            };
+            var project = new Project
+            {
+                Name = "codewithmukesh"
+            };
+
+            _unitOfWork.Developers.Add(developer);
+            _unitOfWork.Projects.Add(project);
+            _unitOfWork.Complete();
+
+            return Ok();
+        }
+
+    }
+}
